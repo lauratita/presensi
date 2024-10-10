9ecc6f95ec151bf8186c989d1b70a62532e9f169
Laura Tita <lauratita316@gmail.com>
2024-10-10 21:04:46 +0700

    menambahkan template web dan file index.php, dashboard.php

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Admin CekInOut</title>
+
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+
+    <!-- Custom styles for this template-->
+    <link href="css/sb-admin-2.css" rel="stylesheet">
+
+</head>
+
+<body class="login-page bg-gradient" style="background-color: #DB7A43">
+
+
+
+    <!-- Outer Row -->
+    <div class="row justify-content-center">
+
+        <div class="col-xl-10 col-lg-12 col-md-9">
+
+            <div class="card o-hidden border-0 shadow-lg my-5">
+                <div class="card-body p-0">
+                    <!-- Nested Row within Card Body -->
+                    <div class="row">
+                        <div class="d-flex justify-content-center align-items-center vh-100 col-lg-6">
+                            <img src="img/Logo.png" alt="Logo" class="img-fluid big-logo">
+                        </div>
+                        <div class="col-lg-6" style="padding-top: 100px;">
+                            <div class="p-5">
+                                <div class="text-center logo-login">
+                                    <h1 class="h4 text-warning-900 mb-4" style="color: #f48a4e"><img src="img/logo2.png"
+                                            alt="">CekInOut
+                                    </h1>
+                                </div>
+                                <form class="user">
+                                    <div class="form-group">
+                                        <input type="email" class="form-control form-control-user"
+                                            id="exampleInputEmail" aria-describedby="emailHelp"
+                                            placeholder="Enter Email Address...">
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="password" class="form-control form-control-user"
+                                            id="exampleInputPassword" placeholder="Password">
+                                    </div>
+                                    <div class="mb-3 text-right">
+                                        <a class="small" href="forgot-password.html" style="color: #f48a4e;">Forgot
+                                            Password?</a>
+                                    </div>
+                                    <a href="dashboard.php" class="btn text-white btn-user btn-block"
+                                        style="background-color: #f48a4e;">
+                                        Login
+                                    </a>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin-2.min.js"></script>
+
+</body>
+
+</html>
