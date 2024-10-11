1a4074504db54000490a6027bdc73c50a5c3eb9f
Laura Tita <lauratita316@gmail.com>
2024-10-11 18:53:10 +0700

    mengelompokkan bagian admin dan guru

--- web/login.php
+++ web/login.php
@@ -12,13 +12,13 @@
     <title>Admin CekInOut</title>
 
     <!-- Custom fonts for this template-->
-    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link href="../vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
 
     <!-- Custom styles for this template-->
-    <link href="css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="../css/sb-admin-2.css" rel="stylesheet">
 
 </head>
 
@@ -36,13 +36,13 @@
                     <!-- Nested Row within Card Body -->
                     <div class="row">
                         <div class="d-flex justify-content-center align-items-center vh-100 col-lg-6">
-                            <img src="img/Logo.png" alt="Logo" class="img-fluid">
+                            <img src="./img/Logo.png" alt="Logo" class="img-fluid">
                         </div>
                         <div class="col-lg-6" style="padding-top: 100px;">
                             <div class="p-5">
                                 <div class="text-center logo-login">
-                                    <h1 class="h4 text-warning-900 mb-4" style="color: #f48a4e"><img src="img/logo2.png"
-                                            alt="">CekInOut
+                                    <h1 class="h4 text-warning-900 mb-4" style="color: #f48a4e"><img
+                                            src="./img/logo2.png" alt="">CekInOut
                                     </h1>
                                 </div>
                                 <form class="user">
@@ -56,10 +56,11 @@
                                             id="exampleInputPassword" placeholder="Password">
                                     </div>
                                     <div class="mb-3 text-right">
-                                        <a class="small" href="forgot-password.html" style="color: #f48a4e;">Forgot
+                                        <a class="small" href="../forgotPassword/forgot-password.php"
+                                            style="color: #f48a4e;">Forgot
                                             Password?</a>
                                     </div>
-                                    <a href="dashboard.php" class="btn text-white btn-user btn-block"
+                                    <a href="guru/index.php" class="btn text-white btn-user btn-block"
                                         style="background-color: #f48a4e;">
                                         Login
                                     </a>
