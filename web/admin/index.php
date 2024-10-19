03da6255e4f835e47001cecc7265cc0ee5e0b1b6
Laura Tita <lauratita316@gmail.com>
2024-10-19 09:25:32 +0700

    header footer admin

--- web/admin/index.php
+++ web/admin/index.php
@@ -1,4 +1,4 @@
-<?php include '../template/header.php'; ?>
+<?php include '../template/headerAdmin.php'; ?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -10,8 +10,8 @@
             <!-- Main Content -->
             <div id="content">
                 <!-- Begin Page Content -->
-                <div class="container-fluid">    
-                <!-- Informasi  -->
+                <div class="container-fluid">
+                    <!-- Informasi  -->
                     <div class="row">
                         <!-- Siswa -->
                         <div class="col-xl-8 col-lg-7">
@@ -20,72 +20,74 @@
                                     <div class="card-body">
                                         <div class="row no-gutters align-items-center">
                                             <div class="col mr-2">
-                                                <div class="h5 font-weight-bold text-warning text-uppercase mb-1 m-0 me-2">Siswa</div>
+                                                <div
+                                                    class="h5 font-weight-bold text-warning text-uppercase mb-1 m-0 me-2">
+                                                    Siswa</div>
                                                 <div class="h2 mb-3 me-2 mt-4 font-weight-bold text-gray-800">1800</div>
                                                 <span>Total Siswa</span>
                                             </div>
                                             <div class="col-auto">
-                                                <i class="fas fa-comments fa-2x text-gray-300"></i>                                
+                                                <i class="fas fa-comments fa-2x text-gray-300"></i>
                                             </div>
                                         </div>
-                                            <ul class="p-0 m-0 mt-5">
-                                                <li class="d-flex mb-4 pb-1">
-                                                    <div class="avatar flex-shrink-0 me-3">
-                                                        <i class="text-success bi bi-person-fill-check mr-2"></i>
+                                        <ul class="p-0 m-0 mt-5">
+                                            <li class="d-flex mb-4 pb-1">
+                                                <div class="avatar flex-shrink-0 me-3">
+                                                    <i class="text-success bi bi-person-fill-check mr-2"></i>
+                                                </div>
+                                                <div
+                                                    class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
+                                                    <div class="me-2">
+                                                        <h6 class="mb-0" style="font-weight: bold;">Hadir</h6>
                                                     </div>
-                                                    <div
-                                                        class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
-                                                        <div class="me-2">
-                                                            <h6 class="mb-0" style="font-weight: bold;">Hadir</h6>
-                                                        </div>
-                                                        <div class="user-progress">
-                                                            <small class="fw-semibold">36</small>
-                                                        </div>
+                                                    <div class="user-progress">
+                                                        <small class="fw-semibold">36</small>
                                                     </div>
-                                                </li>
-                                                <li class="d-flex mb-4 pb-1">
-                                                    <div class="avatar flex-shrink-0 me-3">
-                                                        <i class="text-primary bi bi-person-fill-dash mr-2"></i>
+                                                </div>
+                                            </li>
+                                            <li class="d-flex mb-4 pb-1">
+                                                <div class="avatar flex-shrink-0 me-3">
+                                                    <i class="text-primary bi bi-person-fill-dash mr-2"></i>
+                                                </div>
+                                                <div
+                                                    class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
+                                                    <div class="me-2">
+                                                        <h6 class="mb-0" style="font-weight: bold;">Sakit</h6>
                                                     </div>
-                                                    <div
-                                                        class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
-                                                        <div class="me-2">
-                                                            <h6 class="mb-0" style="font-weight: bold;">Sakit</h6>
-                                                        </div>
-                                                        <div class="user-progress">
-                                                            <small class="fw-semibold">2</small>
-                                                        </div>
+                                                    <div class="user-progress">
+                                                        <small class="fw-semibold">2</small>
                                                     </div>
-                                                </li>
-                                                <li class="d-flex mb-4 pb-1">
-                                                    <div class="avatar flex-shrink-0 me-3">
-                                                        <i class="text-warning bi bi-person-fill-exclamation mr-2"></i>
+                                                </div>
+                                            </li>
+                                            <li class="d-flex mb-4 pb-1">
+                                                <div class="avatar flex-shrink-0 me-3">
+                                                    <i class="text-warning bi bi-person-fill-exclamation mr-2"></i>
+                                                </div>
+                                                <div
+                                                    class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
+                                                    <div class="me-2">
+                                                        <h6 class="mb-0" style="font-weight: bold;">Izin</h6>
                                                     </div>
-                                                    <div
-                                                        class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
-                                                        <div class="me-2">
-                                                            <h6 class="mb-0" style="font-weight: bold;">Izin</h6>
-                                                        </div>
-                                                        <div class="user-progress">
-                                                            <small class="fw-semibold">1</small>
-                                                        </div>
+                                                    <div class="user-progress">
+                                                        <small class="fw-semibold">1</small>
                                                     </div>
-                                                </li>
-                                                <li class="d-flex mb-4 pb-1">
-                                                    <div class="avatar flex-shrink-0 me-3">
-                                                        <i class="text-danger bi bi-person-fill-slash mr-2"></i>
+                                                </div>
+                                            </li>
+                                            <li class="d-flex mb-4 pb-1">
+                                                <div class="avatar flex-shrink-0 me-3">
+                                                    <i class="text-danger bi bi-person-fill-slash mr-2"></i>
+                                                </div>
+                                                <div
+                                                    class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
+                                                    <div class="me-2">
+                                                        <h6 class="mb-0" style="font-weight: bold;">Alpha</h6>
                                                     </div>
-                                                    <div
-                                                        class="d-flex w-100 flex-wrap align-items-center justify-content-between gap-2">
-                                                        <div class="me-2">
-                                                            <h6 class="mb-0" style="font-weight: bold;">Alpha</h6>
-                                                        </div>
-                                                        <div class="user-progress">
-                                                            <small class="fw-semibold">1</small>
-                                                        </div>
+                                                    <div class="user-progress">
+                                                        <small class="fw-semibold">1</small>
                                                     </div>
-                                                </li>
-                                            </ul>
+                                                </div>
+                                            </li>
+                                        </ul>
                                     </div>
                                 </div>
                             </div>
@@ -99,7 +101,9 @@
                                     <div class="card-body">
                                         <div class="row no-gutters align-items-center">
                                             <div class="col mr-2">
-                                                <div class="h5 font-weight-bold text-warning text-uppercase mb-1 m-0 me-2">Kelas</div>
+                                                <div
+                                                    class="h5 font-weight-bold text-warning text-uppercase mb-1 m-0 me-2">
+                                                    Kelas</div>
                                                 <div class="h2 mb-3 me-2 mt-4 font-weight-bold text-gray-800">24</div>
                                             </div>
                                             <div class="col-auto">
@@ -110,22 +114,24 @@
                                 </div>
                             </div>
                             <!-- Pegawai -->
-                                <div class="card shadow mb-4">
-                                    <div class="card border-left-warning shadow h-100 py-2">
-                                        <div class="card-body">
-                                            <div class="row no-gutters align-items-center">
-                                                <div class="col mr-2">
-                                                    <div class="h5 font-weight-bold text-warning text-uppercase mb-1 m-0 me-2">Pegawai</div>
-                                                    <div class="h2 mb-3 me-2 mt-4 font-weight-bold text-gray-800">500</div>
-                                                </div>
-                                                <div class="col-auto">
-                                                    <i class="fas fa-comments fa-2x text-gray-300"></i>
-                                                </div>
+                            <div class="card shadow mb-4">
+                                <div class="card border-left-warning shadow h-100 py-2">
+                                    <div class="card-body">
+                                        <div class="row no-gutters align-items-center">
+                                            <div class="col mr-2">
+                                                <div
+                                                    class="h5 font-weight-bold text-warning text-uppercase mb-1 m-0 me-2">
+                                                    Pegawai</div>
+                                                <div class="h2 mb-3 me-2 mt-4 font-weight-bold text-gray-800">500</div>
+                                            </div>
+                                            <div class="col-auto">
+                                                <i class="fas fa-comments fa-2x text-gray-300"></i>
                                             </div>
                                         </div>
                                     </div>
-                                </div>                                
-                        </div> 
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <!-- Content Row -->
 
