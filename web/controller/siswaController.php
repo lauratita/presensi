4a23a2cfb1b73993e9598da4ca4acb37cc7c8401
itanrll <itanur246@gmail.com>
2024-12-04 15:16:37 +0700

    otomatis naik kelas

--- web/controller/siswaController.php
+++ web/controller/siswaController.php
@@ -19,16 +19,27 @@ class SiswaController{
             $jenis_kelamin = $request['jenis_kelamin'];
             $alamat = $request['alamat'];
             $foto = $_FILES['foto'];
+            $tanggal_masuk = $request['tanggal_masuk'];
+            $tanggal_akhir = $request['tanggal_berakhir'];
             $id_kelas = $request['id_kelas'];
             $nik_ortu = $request['nik_ortu'];
             
             var_dump($request);
-            if ($this->siswaService->createSiswa($nis, $nama, $tanggal_lahir, $tahun_akademik, $password, $jenis_kelamin, $alamat, $foto, $id_kelas, $nik_ortu)) {
+            if ($this->siswaService->createSiswa($nis, $nama, $tanggal_lahir, $tahun_akademik, $password, $jenis_kelamin, $alamat, $foto, $tanggal_masuk, $tanggal_akhir, $id_kelas, $nik_ortu)) {
                 return json_encode(["message" => "Berhasil tambah data"]);
             }
             return json_encode(["message" => "Gagal menambah data"]);
         } catch (Exception $e) {
             return json_encode(["message" => $e->getMessage()]);
+        }
+    }
+
+    public function naikKelas() {
+        $result = $this->siswaService->naikKelas();
+        if ($result) {
+            echo json_encode(["message" => "Naik kelas berhasil"]);
+        } else {
+            echo json_encode(["message" => "Gagal naik kelas"]);
         }
     }
 
@@ -50,9 +61,11 @@ class SiswaController{
         $jenis_kelamin = $request['edit_jenis_kelamin'];
         $alamat = $request['edit_alamat'];
         $foto = $_FILES['edit_foto'];
+        $tanggal_masuk = $request['edttanggal_masuk'];
+        $tanggal_akhit = $request['edttanggal_berakhir'];
         $id_kelas = $request['edit_id_kelas'];
         $nik_ortu = $request['edit_nik_ortu'];
-        $data = $this->siswaService->updateSiswa($nis, $nama, $tanggal_lahir, $tahun_akademik, $jenis_kelamin, $alamat, $foto, $id_kelas, $nik_ortu);
+        $data = $this->siswaService->updateSiswa($nis, $nama, $tanggal_lahir, $tahun_akademik, $jenis_kelamin, $alamat, $foto, $tanggal_masuk, $tanggal_akhit, $id_kelas, $nik_ortu);
         if ($data) {
             // var_dump($data);
             return json_encode(["message" => "Berhasil Perbarui Siswa"]);
